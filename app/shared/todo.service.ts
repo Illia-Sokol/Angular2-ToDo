370a14b377bae5b5fceec77b5be95340c400987e
Illia Sokol <illiasokolovskyi@gmail.com>
2016-08-27 00:07:17 +0300

    added TodoService

---
+++ app/shared/todo.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+import { ITodo } from './todo.model';
+import { todos } from './todo.data';
+
+@Injectable()
+
+export class TodoService {
+    getTodos(): ITodo[] {
+        return todos;
+    }
+}
