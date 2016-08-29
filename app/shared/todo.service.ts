28e7966848b29d6c5947e47f008d5a7a0d49966c
Illia Sokol <illiasokolovskyi@gmail.com>
2016-08-29 21:43:34 +0300

    add service asynhronously

--- app/shared/todo.service.ts
+++ app/shared/todo.service.ts
@@ -6,7 +6,7 @@ import { todos } from './todo.data';
 @Injectable()
 
 export class TodoService {
-    getTodos(): ITodo[] {
-        return todos;
+    getTodos(): Promise<ITodo[]> {
+        return new Promise(resolve => setTimeout( () => resolve(todos), 2000));
     }
 }
